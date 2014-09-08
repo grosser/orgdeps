d4524283e4009ab21df8128dca35158e4a223178
grosser <michael@grosser.it>
2014-09-07 22:08:07 -0700

    we do not need repo access

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ require 'omniauth-github'
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_SECRET"], :scope => "user,repo,read:org"
+  provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_SECRET"], :scope => "user,read:org"
 end
