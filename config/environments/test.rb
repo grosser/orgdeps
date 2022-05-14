147743f618e401ef854202f4d6fd296dd0ac0264
Michael Grosser <michael@grosser.it>
2022-05-14 10:37:38 -0700

    remove extra rails parts

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  # config.action_mailer.delivery_method = :test
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
