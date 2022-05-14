b87ddea4b372c473598fbf2d1157c287827faf0a
Michael Grosser <michael@grosser.it>
2022-05-14 10:33:14 -0700

    allow blocking edit

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,10 +4,12 @@ class OrganizationsController < ApplicationController
   end
 
   def edit
+    return if ENV["EDIT_BLOCKED"]
     organization
   end
 
   def update
+    return if ENV["EDIT_BLOCKED"]
     organization.update!(params.require(:organization).permit(:safe_github_token))
     redirect_to organization_repositories_path(organization)
   end
